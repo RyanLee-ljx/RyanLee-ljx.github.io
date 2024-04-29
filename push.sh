a8c161ff19840b6ba1d54fce023c35b5cbbdea92
Flame <2284771024@qq.com>
2024-04-29 19:35:16 +0800

    deploy:20240429-19:35:15

---
+++ push.sh
@@ -0,0 +1,4 @@
+git checkout image
+git add -A   # 把所有本地文件（当前文件夹）添加到暂存区
+git commit -m 'deploy:'$(date "+%Y%m%d-%H:%M:%S")    # 提交  后面是注释，表示当前时间
+git push origin image   # origin远程仓库名字  main 仓库下分支名字 
